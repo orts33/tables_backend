aedb257cb1b5214e78e885a9241ac92ae29c8241
Vladislav <wa666pa666@gmail.com>
2025-07-11 10:12:36 +0300

    SSL added

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as fs from 'fs';
+import * as process from 'process';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const useHttps = process.env.USE_HTTPS === 'true';
+  const sslKeyPath = process.env.SSL_KEY_PATH;
+  const sslCertPath = process.env.SSL_CERT_PATH;
+
+  let app;
+
+  if (useHttps && fs.existsSync(sslKeyPath) && fs.existsSync(sslCertPath)) {
+    const httpsOptions = {
+      key: fs.readFileSync(sslKeyPath),
+      cert: fs.readFileSync(sslCertPath),
+    };
+    app = await NestFactory.create(AppModule, { httpsOptions });
+    console.log('✅ Запущен в режиме HTTPS');
+  } else {
+    app = await NestFactory.create(AppModule);
+    console.log('⚠️ Запущен в режиме HTTP');
+  }
 
   app.enableCors({
     origin: 'http://localhost:5173',
-    credentials: true, // если используешь куки или авторизацию
+    credentials: true,
   });
 
   const config = new DocumentBuilder()
@@ -17,6 +35,7 @@ async function bootstrap() {
       .addTag('tables', 'Операции со столами')
       .addTag('participants', 'Операции с участниками')
       .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document, {
     swaggerOptions: {
